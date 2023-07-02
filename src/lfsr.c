cfc035d22cf6e9f85830a60267ad8008fb8bec4e
Quentin Patinier <quentin.patinier@gmail.com>
2023-07-02 08:15:25 +0200

    added lfsr

---
+++ src/lfsr.c
@@ -0,0 +1,29 @@
+#include "rng.h"
+
+
+// RNG seed
+extern uint64_t seed;
+
+
+// LFSR parameters
+static uint64_t lfsr_poly = 0x80000000000019E2;
+
+static uint8_t rng_lfsr_bit(){
+    uint8_t bit = seed & 1;
+
+    for (int i = 1; i < 64; i++){
+        if ((lfsr_poly >> i) & 1)
+            bit ^= (seed >> i);
+    }
+
+    bit &= 1;
+    seed = seed >> 1 | ((uint64_t) bit << 63);
+    return bit;
+}
+
+uint32_t rng_lfsr(){
+    uint32_t num = 0;
+    for (int i = 0; i < 32; i++)
+        num = num << 1 | rng_lfsr_bit();
+    return num;
+}
--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@ int main(){
 
   rng_set_seed(0x1000);
 
-  for (int i = 0; i < 20; i++){
-    printf("0x%x\n", rng_lcg());
+  for (int i = 0; i < 60; i++){
+    printf("0x%x\n", rng_lfsr());
   }
   
   return 0;
